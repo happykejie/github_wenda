58145fbb474db9326752764a528c7dca00a24fae
zhangjie <happykejie@126.com>
2016-09-04 01:28:04 +0800

    问答项目
    中际离职前，问答项目——她他好孕和博士知道最新代码 2016年9月4日上传

--- index.php
+++ index.php
@@ -4,32 +4,30 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-define("APP_ROOT",dirname(__FILE__));
-
 
 
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
 
 
-$domain =  $_SERVER['HTTP_HOST'];//鑾峰彇褰撳墠鍩熷悕
+$domain =  $_SERVER['HTTP_HOST'];//获取当前域名
 
 
 if($domain =='www.tatahaoyun.com')
 {
-    require(__DIR__ . '/config/tatahaoyunconfig.php'); ///寮曞叆".DOMAITDESC."鍏叡甯搁噺
+    require(__DIR__ . '/config/tatahaoyunconfig.php'); ///引入".DOMAITDESC."公共常量
 }
 
 
 
 else if($domain =='www.boshizhidao.com')
 {
-    require(__DIR__ . '/config/boshizhidaoconfig.php'); ///寮曞叆鍗氬＋鐭ラ亾鍏叡甯搁噺
+    require(__DIR__ . '/config/boshizhidaoconfig.php'); ///引入博士知道公共常量
 }
 
 else
 {
-    require(__DIR__ . '/config/cuxiaodadiaochaconfig.php'); ///寮曞叆榛樿鍏叡甯搁噺
+    require(__DIR__ . '/config/boshizhidaoconfig.php'); ///引入默认公共常量
 }
 
 
@@ -39,6 +37,3 @@ define('BASE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
 $config = require(__DIR__ . '/config/web.php');
 
 (new yii\web\Application($config))->run();
-
-?>
-
